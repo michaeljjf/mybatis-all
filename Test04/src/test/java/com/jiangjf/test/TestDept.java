f27ad59d8837466d3e9728df63512f15bd21cd8f
jiangjf <34030327@qq.com>
2021-06-12 20:36:45 +0800

    多表查询——级联延迟加载

--- Test04/src/test/java/com/jiangjf/test/TestDept.java
+++ Test04/src/test/java/com/jiangjf/test/TestDept.java
@@ -2,13 +2,19 @@ package com.jiangjf.test;
 
 import com.jiangjf.mapper.DeptMapper;
 import com.jiangjf.pojo.Dept;
+import com.jiangjf.pojo.Emp;
 import org.junit.Test;
+
+import java.util.List;
 
 public class TestDept extends TestBase {
     @Test
     public void testFindDeptByDeptno() {
         DeptMapper deptMapper = session.getMapper(DeptMapper.class);
         Dept dept = deptMapper.findDeptByDeptno(20);
-        System.out.println(dept);
+        System.out.println(dept.getDname());
+        System.out.println(dept.getLoc());
+//        List<Emp> empList = dept.getEmpList();
+//        empList.forEach(System.out::println);
     }
 }
